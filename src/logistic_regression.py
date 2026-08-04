c108fbcc68397227e12b8b945e043eb341c4d0ea
pachiderms <zizi.telvin@gmail.com>
2026-08-04 16:49:47 +0200

    reg_logistic_regression

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -2,18 +2,22 @@ import numpy as np
 from matplotlib import pyplot as plt
 from src.log_pred import logistic_predict_
 from src.log_gradient import log_gradient
+from src.logistic_loss_reg import reg_log_loss_
+from src.reg_logistic_grad import vec_reg_logistic_grad
 from src.vec_log_loss import vec_log_loss_
 from src.decorators import check_type_and_shape_fit_method, check_type_and_shape_vector_pair_any
 
 class MyLogisticRegression():
     """
     Description:
-    My personnal multiple linear regression class to fit like a boss.
+    My personnal logistic regression to classify things.My personnal logistic regression to classify things.
     """
-    def __init__(self, thetas, alpha=0.001, max_iter=1000):
+    def __init__(self, thetas, alpha=0.001, max_iter=1000, penality='l2', lambda_=1.0):
         self.alpha = alpha
         self.max_iter = max_iter
         self.thetas = np.asarray(thetas, dtype=float).reshape(-1, 1)
+        self.penality = penality if penality == 'l2' else None
+        self.lambda_ = lambda_ if self.penality == 'l2' else 0.0
         self.costs = []
         
     @check_type_and_shape_fit_method
@@ -26,7 +30,7 @@ class MyLogisticRegression():
             current_loss = self.loss_(y, y_hat)
             self.costs.append(current_loss)
                             
-            grad = log_gradient(x, y, new_theta)
+            grad = log_gradient(x, y, new_theta) if self.penality == None else vec_reg_logistic_grad(y, x, self.thetas, self.lambda_)
             new_theta = new_theta - self.alpha * grad
 
         self.thetas = new_theta
@@ -36,6 +40,7 @@ class MyLogisticRegression():
         plt.figure()
         plt.xlabel("iterations")
         plt.ylabel("J(w, b)")
+        plt.title(f"Loss for lambda={self.lambda_}")
 
         plt.plot(self.costs, label='learning curve')
         plt.legend()
@@ -50,7 +55,7 @@ class MyLogisticRegression():
         return J_elem
 
     def loss_(self, y, y_hat):
-        return vec_log_loss_(y, y_hat)
+        return vec_log_loss_(y, y_hat) if self.penality == None else reg_log_loss_(y, y_hat, self.thetas, self.lambda_)
 
     @check_type_and_shape_vector_pair_any
     def mse_(self, y, y_hat):
